d6b133d9b8e84c0f3b099e8719190ae31e3a11ca
Arman Nikoyan <armannikoyan@Armans-Air.lan>
2024-03-05 21:03:47 +0400

    dotfiles v2

---
+++ nvim/lua/namra/lazy.lua
@@ -0,0 +1,40 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+	},
+	{ "nvim-treesitter/nvim-treesitter" },
+	{
+		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		}
+	},
+	{ "mbbill/undotree" },
+	{ "tpope/vim-fugitive" },
+	{ "nvim-treesitter/nvim-treesitter-context" },
+})
---
+++ nvim/lua/namra/set.lua
@@ -0,0 +1,20 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.termguicolors = true
+
+vim.opt.list = true
+vim.opt.listchars = {
+	tab = ' ',
+	extends = '…',
+	precedes = '…',
+	nbsp = '␣',
+	trail = '•',
+	-- eol = '↲ ',
+}
+
+vim.opt.colorcolumn = "80"
