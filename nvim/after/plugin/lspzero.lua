7307a9769a48e42cb80fd8d846e4b548d0f55997
Arman Nikoyan <armannikoyan@Armans-MacBook-Air.local>
2024-03-06 12:23:02 +0400

    Remap changes

--- nvim/after/plugin/lspzero.lua
+++ nvim/after/plugin/lspzero.lua
@@ -33,9 +33,6 @@ cmp.setup({
 		-- `Enter` key to confirm completion
 		['<CR>'] = cmp.mapping.confirm({select = false}),
 
-		-- Ctrl+Space to trigger completion menu
-		['<C-cm>'] = cmp.mapping.complete(),
-
 		-- Navigate between snippet placeholder
 		['<C-f>'] = cmp_action.luasnip_jump_forward(),
 		['<C-b>'] = cmp_action.luasnip_jump_backward(),
