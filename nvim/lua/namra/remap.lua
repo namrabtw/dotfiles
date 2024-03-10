95c9707383225a65198ce75c83cc3f7ab089950a
Arman Nikoyan <armannikoyan@proton.me>
2024-03-10 13:59:20 +0400

    Some minor but usefull changes

--- nvim/lua/namra/remap.lua
+++ nvim/lua/namra/remap.lua
@@ -2,14 +2,10 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("v", "C-j", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "C-k", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set({"n", "i", "v"}, "<C-c>", "<NOP>")
-vim.keymap.set({"i"}, "jk", "<Esc>")
-vim.keymap.set({"i"}, "kj", "<Esc>")
 
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
